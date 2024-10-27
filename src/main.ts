66cf1d9fe1f6ba55e8c10aa56b42280865b95d24
ateklyuk <159532812+ateklyuk@users.noreply.github.com>
2024-10-27 08:22:44 +0300

    docs

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     AppModule,
     {
       options: {
-        host: 'localhost',
+        host: '0.0.0.0',
         port: 5052,
       },
       transport: Transport.TCP,
